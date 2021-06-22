a106eb909c5c4335b4fc7734622e837c7392825e
LaraMatos <56411039+laramatos22@users.noreply.github.com>
2021-06-22 14:03:15 +0100

    Update ex1.c

--- aulatp09/Parte1/ex1/ex1.c
+++ aulatp09/Parte1/ex1/ex1.c
@@ -5,8 +5,8 @@
 // transmissão da UART2:
 #define DisableUart2RxInterrupt() IEC1bits.U2RXIE = 0
 #define EnableUart2RxInterrupt() IEC1bits.U2RXIE = 1
-#define DisableUart2TxInterrupt() IEC1bits.U2RXIE = 0
-#define EnableUart2TxInterrupt() IEC1bits.U2RXIE = 1
+#define DisableUart2TxInterrupt() IEC1bits.U2TXIE = 0
+#define EnableUart2TxInterrupt() IEC1bits.U2TXIE = 1
 
 //--------------
 // 2. Declare a estrutura que implementa um buffer circular e crie duas instâncias dessa
